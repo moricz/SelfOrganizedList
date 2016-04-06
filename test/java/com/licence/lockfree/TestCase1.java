f00c2443d4d3354b2c9d031ae3c5527d583cbb96
moricz <moricz.sandor@yahoo.com>
2016-04-06 16:55:06 +0300

    Added files via upload

--- test/java/com/licence/lockfree/TestCase1.java
+++ test/java/com/licence/lockfree/TestCase1.java
@@ -1,72 +1,50 @@
-package com.licence.lockfree;
-
+package tests;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import com.licence.lockfree.selfOrganizing.SelfOrganizingList;
+import lockfree.selfOrganizing.SelfOrgList;
 
-import TrialSuite.TrialSuite;
+public class TestCase1 {
 
+	SelfOrgList sol;
+	Long start = (long) 0, end = (long) 0;
+	int i = 0;
+	static Long len = (long) 0;
 
-
-public class TestCase1  {
-
-	SelfOrganizingList sol;
-	Long start=(long)0,end=(long)0;
-	 int i=0;
-	 static Long len=(long) 0;
-	 TrialSuite ts=new TrialSuite();
 	@BeforeTest
 	protected void setUp() throws Exception {
-		
-		
-		sol=new SelfOrganizingList();
-		
+
+		sol = new SelfOrgList();
+
 		System.gc();
-		start= System.nanoTime();
-		
-		sol.add(5);
-		sol.add(5);
-		sol.add(5);
-		sol.add(5);
-		sol.add(5);
+
+		for (int i = 0; i < 101; i++)
+			sol.add(10);
+
 		sol.add(7);
+		start = System.nanoTime();
 	}
 
 	@AfterTest
 	protected void tearDown() throws Exception {
 		end = System.nanoTime();
-		
 		sol.list();
-	    double seconds = (end-start)/1000000000.0;
-		System.out.println("Time: "+ seconds +"s  Size: " + sol.size());
-	
-	//	System.out.println(ts.histogram());
+		double seconds = (end - start) / 1000000000.0;
+		System.out.println("Time: " + seconds + "s Size: " + sol.size());
+
 	}
-	
-	
-	
-	
-	@Test(threadPoolSize = 10, invocationCount = 50, timeOut=10000)
-	public void test2(){
-		
-		
+
+	@Test(threadPoolSize = 15, invocationCount = 100, timeOut = 10000)
+	public void test2() {
+
 		sol.add(10);
-		
-		
-		//sol.remove(10);
-	
-		
-		//for(i=0;i<2;i++)
-	//	sol.search(7);
-	
-	//	long end =System.currentTimeMillis();
-	//	ts.addTrial(TestCase1.len++,start,end);
-		
+
+		// sol.remove(10);
+		// sol.remove(10);
+		sol.search(7);
+
 	}
-	
-	
 
 }
